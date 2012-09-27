5ccd62f81027d761fc6feb0c4ec63ec1da3d0f35
itsnilay <itsnilay@564bbc49-d03c-ec2e-1046-c5b1e0dfa69f>
2012-09-27 23:49:54 +0000

    Manifest changes

--- trunk/gen/com/taulia/hackathon/R.java
+++ trunk/gen/com/taulia/hackathon/R.java
@@ -12,9 +12,22 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_launcher1=0x7f020001;
+    }
+    public static final class id {
+        public static final int amtTextView=0x7f050001;
+        public static final int btnChangeDate=0x7f050006;
+        public static final int buyerTextView=0x7f050003;
+        public static final int dscDateDP=0x7f050004;
+        public static final int dueDateTextView=0x7f050002;
+        public static final int epinvlist=0x7f050007;
+        public static final int invNoTextView=0x7f050000;
+        public static final int tvDate=0x7f050005;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int desc=0x7f030000;
+        public static final int ep_inv=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
