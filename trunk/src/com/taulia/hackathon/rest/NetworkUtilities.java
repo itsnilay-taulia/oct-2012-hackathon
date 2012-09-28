6e08d2254d2d5b0efe701309e05a1d69be9f2461
mzeilenga@gmail.com <mzeilenga@gmail.com@564bbc49-d03c-ec2e-1046-c5b1e0dfa69f>
2012-09-28 01:42:41 +0000

    Fixes

--- trunk/src/com/taulia/hackathon/rest/NetworkUtilities.java
+++ trunk/src/com/taulia/hackathon/rest/NetworkUtilities.java
@@ -2,14 +2,11 @@ package com.taulia.hackathon.rest;
 
 import android.util.Log;
 import java.io.*;
-import java.util.ArrayList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.params.ConnManagerParams;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -21,7 +18,7 @@ import org.w3c.dom.Document;
  * @author Matt Zeilenga
  */
 public class NetworkUtilities {
-  private static final String LOG_TAG = "NetworkUtilities";
+//  private static final String LOG_TAG = "NetworkUtilities";
 
   public static final String PROJECT_URI = "http://72.167.4.179/taulia_android_app/index.php";
 
@@ -40,40 +37,32 @@ public class NetworkUtilities {
     return httpClient;
   }
 
-  public static HttpResponse post(ArrayList<NameValuePair> params)
+  public static HttpResponse post(String uriExt)
   throws IOException, IllegalStateException {
 
-    final HttpEntity entity;
-
-    try {
-      entity = new UrlEncodedFormEntity(params);
-    } catch (final UnsupportedEncodingException e) {
-      throw new IllegalStateException(e);
-    }
-    final HttpPost post = new HttpPost(PROJECT_URI);
-    post.addHeader(entity.getContentType());
-    post.setEntity(entity);
+    final HttpPost post = new HttpPost(PROJECT_URI.concat(uriExt));
 
     return getHttpClient().execute(post);
   }
 
 
-  public static Document doHttpPost(ArrayList<NameValuePair> params) {
+  public static Document doHttpPost(String uriExt) {
     HttpResponse resp;
 
     try {
-      resp = NetworkUtilities.post(params);
-
-      InputStream is = resp.getEntity().getContent();
+      resp = NetworkUtilities.post(uriExt);
+      HttpEntity ent = resp.getEntity();
+      InputStream is = ent.getContent();
 
       DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+      dbFactory.setIgnoringElementContentWhitespace(true);
       DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
       Document doc = dBuilder.parse(is);
-      doc.getDocumentElement().normalize();
+      System.out.println(doc);
       return doc;
     }
     catch (Exception ex) {
-      Log.e(LOG_TAG, "Exception occured when getting content from POST", ex);
+//      Log.e(LOG_TAG, "Exception occured when getting content from POST", ex);
       return null;
     }
   }
